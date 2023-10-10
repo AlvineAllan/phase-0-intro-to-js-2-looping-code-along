bb48a89f0ebb61e000da2d9103e2f585f4865928
Alvine Allan <alvizallaz@gmail.com>
2023-10-11 01:38:19 +0300

    Completed Assignent

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, surprise){
+   for(let i=0;i<=names.length;i++){
+    console.log(`Thank you ${names[i]}for the wonderful surprise gift!`);
+    debugger;
+   }
+   return surprise =[`Thank you, ${names[0]}, for the wonderful surprise gift!`, `Thank you, ${names[1]}, for the wonderful surprise gift!`, `Thank you, ${names[2]}, for the wonderful surprise gift!`];
+}
+function countDown(){
+    let num = 10;
+    while(num>(-1)){
+        console.log(num--);
+    }
+  return num;
+}
